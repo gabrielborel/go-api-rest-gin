253ed5aec661115ba991a0ce923e6a864a2c9fbb
Gabriel Borel <biel_borel@hotmail.com>
2022-10-08 21:47:47 -0300

    Added some validation to the student struct and created a validate func

--- models/student.go
+++ models/student.go
@@ -1,11 +1,23 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
 type Student struct {
 	gorm.Model
 	
-	Name 	string `json:"name"`
-	CPF		string `json:"cpf"`
-	RG 		string `json:"rg"`
+	Name 	string `json:"name" validate:"nonzero"`
+	RG 		string `json:"rg" validate:"len=9, regexp=^[0-9]*$"`
+	CPF		string `json:"cpf" validate:"len=11, regexp=^[0-9]*$"`
+}
+
+func ValidateStudentData(student *Student) error {
+	err := validator.Validate(student)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
